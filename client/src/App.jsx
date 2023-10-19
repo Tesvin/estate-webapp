ec25d673ffd9753a778354f6dfdfcd23d201803e
Tesvin <stephenazosike@gmail.com>
2023-10-19 10:23:44 +0100

    updated signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import { Header } from './components/Header';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
