066d92590e7fc48e95c05d362e28a4b72c3a0a70
Tesvin <stephenazosike@gmail.com>
2023-10-19 10:06:55 +0100

    created middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -21,6 +21,16 @@ mongoose
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 
+//middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server is runing on port 3000')
