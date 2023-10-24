8db83106481bdccea882c02a343d12b67e586963
Tesvin <stephenazosike@gmail.com>
2023-10-24 09:46:14 +0100

    completed update user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     currentUser: null,
-    erro: null,
+    error: null,
     loading: false,
 };
 
@@ -21,10 +21,29 @@ const userSlice = createSlice({
         signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
-        }
-    }
-})
+        },
+        updateUserStart: (state) => {
+            state.loading = true;
+          },
+          updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+          },
+          updateUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+          },
+    },
+});
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const { 
+    signInStart, 
+    signInSuccess, 
+    signInFailure, 
+    updateUserFailure, 
+    updateUserStart, 
+    updateUserSuccess 
+} = userSlice.actions;
 
 export default userSlice.reducer
