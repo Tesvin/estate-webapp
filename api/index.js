ca7b0d3ba29c695c41a49cf7c9ecaec2616e3971
Tesvin <stephenazosike@gmail.com>
2023-10-24 08:15:32 +0100

    created user update api

--- api/index.js
+++ api/index.js
@@ -3,12 +3,25 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
-const app = express()
+const app = express();
 
+// inbuilt middleware
 app.use(express.json());
+app.use(cookieParser());
 
+// middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
 mongoose
     .connect(process.env.MONGO)
@@ -22,16 +35,6 @@ mongoose
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 
-//middleware
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
-});
 
 app.listen(3000, () => {
     console.log('Server is runing on port 3000')
